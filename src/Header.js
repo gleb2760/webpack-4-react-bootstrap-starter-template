28de24943778c76727a1bd87ce4f1e0e7237d8ee
Gleb <glebmelnikov46@gmail.com>
2020-04-16 07:22:17 +0300

    1

---
+++ src/Header.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import logo from "./image/logo.png";
+
+const Header =()=>{
+	return (
+			<div className="header">
+				<div class="container-fuild">
+					<div class="row row_dop" >
+						<div class="col-2">
+							<img class="logo " src={logo}/>
+						</div>
+						<div class="artical row d-flex m-2 right" >
+						<div class="col-10">
+							<button class="btn btn-rounded btn-primary ml-5 mt-2">
+							заказать проект
+							</button> 	
+							<div class="round round_dop">
+							
+							</div>	
+							<button class="btn btn-rounded btn-primary right call">
+							обратный звонок
+							</button> 
+							<h3 class="centered centered_dop">О НАС
+							</h3>
+							<p class="discription_right_first">
+							Описание Описание Описание Описание
+							Описание Описание Описание Описание
+							</p>
+						</div>
+						<div class="col-2 header_1">
+							<div class="round header_2">
+							
+							</div>
+							<div class="round header_3">
+							
+							</div>
+							<div className="header_4">
+							<span className="header_5"> Аг
+							</span>
+							</div>
+							<div className="round header_6">
+							
+							</div>
+						</div>
+						</div>
+					</div>
+				</div>
+			</div>
+	);
+}
+
+export default Header;
---
+++ src/Main.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import photo from "./image/logo.png";
+
+const Main =()=>{
+    return(
+                <div className="main container-fuild">
+                    <div className="row ml-3 mr-3 main_1">
+                        <nav>	
+                            <ul className="aside aside_dop">
+                                <li>
+                                    <div className="round main_2"></div>
+                                    <div className="main_3">
+                                        <p className="main_4"> <a href="#">
+                                            Портфило</a>
+                                        </p>
+                                    </div>
+                                    <div className="round main_5"></div>
+                                </li>
+
+                                <li className="main_6">
+                                    <div className="round"></div>
+                                    <div className="main_7">
+                                        <span className="main_8"><a href="#">Контакты</a>
+                                        </span>
+                                    </div>
+                                    <div className="round"></div>
+                                </li>
+
+                                <li className="main_16">
+                                    <div className="round"></div>
+                                    <div className="main_7" >
+                                        <span className="main_9"><a href="#">Агенство</a>
+                                        </span>
+                                    </div>
+                                    <div className="round"></div>
+                                </li>
+
+                                <li className="main_17">
+                                    <div className="round"></div>
+                                    <div className="main_10">
+                                        <span className="main_11"> Аг
+                                        </span>
+                                    </div>
+                                    <div className="round main_12"></div>
+                                    <div className="round main_13"></div>
+                                    <div className="main_14"></div>
+                                </li>
+                            </ul>
+                        </nav>													
+                <div className="row">
+                    <div className="discription_right" > 
+                    <p>
+                        Описание Описание Описание Описание
+                        Описание Описание Описание Описание
+                        Описание Описание Описание Описание
+                        Описание Описание Описание Описание
+                        Описание Описание Описание Описание
+                        Описание Описание Описание Описание
+                        Описание Описание Описание Описание
+                        Описание Описание Описание Описание
+                        Описание Описание Описание Описание
+                        Описание Описание Описание Описание
+                        Описание Описание Описание Описание
+                        Описание Описание Описание Описание		
+                        Описание
+                        </p>				
+                        <h3 className="main_15">Крайний проект</h3>				
+                        <div className="row ">
+                            <div className="col-4">
+                                <img className="photo" src={photo} alt="picture"/>
+                            </div>
+                            <div className="col-8">
+                                <p className="site">Cайт для компании "прометрей"</p>
+                                <button btn-rounded className="btn btn-rounded btn-primary">
+                                    посмотреть
+                                </button> 
+                            </div>
+                        </div>
+                    </div>
+                    <div class="text3 phone">
+                                    х(ххх)хххх
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Main;
